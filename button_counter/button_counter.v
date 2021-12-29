6fe8a093b6199d027d2f7311c60657fff0b3c78f
David Neumann <davidjneumann@gmail.com>
2021-12-29 05:10:27 -0500

    Add not-working first attempt at exercise

--- button_counter/button_counter.v
+++ button_counter/button_counter.v
@@ -3,26 +3,36 @@
 module button_counter (
 
     //Inputs
-    input        [1:0]   pmod,
+    input                pmod,
+    input                clk,
 
     //Outputs
     output  reg  [3:0]   led
 );
 
     wire rst;
+    reg [31:0] divider;
+    reg div_clk;
 
     // Reset is the inverse of the first button
-    assign rst = ~pmod[0];
+    assign rst = ~pmod;
 
-    // Clock is the inverse of the second button
-    assign clk = ~pmod[1];
-
+    always @(posedge clk or posedge rst) begin
+        if (rst == 1'b1) begin
+            divider <= 32'b0;
+        end else if (divider == 600000) begin
+            div_clk <= ~div_clk;
+        end else begin
+            divider <= divider + 1;
+        end
+    end
     // Count up on clock rising edge or reset on reset button push
-    always @ (posedge clk or posedge rst) begin
+    always @ (posedge div_clk or posedge rst) begin
         if (rst == 1'b1) begin
             led <= 4'b0;
         end else begin
             led <= led + 1'b1;
         end
     end
+
 endmodule
